08260a453960d346b6c194f86ae4fec8fb03a08c
avokeys <alexander.ayala.palacin@gmail.com>
2022-02-01 01:51:32 +0000

    finished domain generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let diffDomain = [".com", ".url", ".io"];
+let domain = [];
+window.onload = function() {};
+let newDomain;
+for (let itemP of pronoun) {
+  for (let itemA of adj) {
+    for (let itemN of noun) {
+      for (let itemD of diffDomain) {
+        newDomain = itemP + itemA + itemN + itemD;
+        domain.push(newDomain);
+      }
+    }
+  }
+}
+
+//  `` thegreatjogger.com
+// thegreatracoon.com
+// ourgreatjogger.com
+// ourgreatracoon.com
+// thebigjogger.com
+// thebigracoon.com
+// ourbigjogger.com
+// ourbigracoon.com``
